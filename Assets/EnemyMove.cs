d4db994f0f84ac3e8d493fd6ef61f1630e07e170
HirokiYamamoto <hiroki200328@icloud.com>
2024-04-24 07:10:59 +0900

    範囲内にプレイヤーが来た場合追っかける ついでに通常(ランダム)移動している場合に進行方向に向くように

--- Assets/EnemyMove.cs
+++ Assets/EnemyMove.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-	[SerializeField] float speed;
+	[SerializeField] float normalSpeed;
+	[SerializeField] float lookSpeed;
 
 	Rigidbody2D rb;
+
+	public bool isLookPlayer;
 
 	float time;
 	[SerializeField] float moveChangeTime;
@@ -14,6 +17,10 @@ public class EnemyMove : MonoBehaviour
 	Vector2 randomSpeed;
 
 	[SerializeField] EnemyHp enemyHp;
+
+	[SerializeField] GameObject playerObj;
+
+	Vector3 toDirection;
 
 	// Start is called before the first frame update
 	void Start()
@@ -26,39 +33,59 @@ public class EnemyMove : MonoBehaviour
 		randomSpeed = randomSpeed.normalized;
 		var v = rb.velocity;
 
-		v = randomSpeed * speed;
+		v = randomSpeed * normalSpeed;
 
 		rb.velocity = v;
+
+		playerObj = GameObject.Find("Player");
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if(isLookPlayer)
+		{
+			// 対象物へのベクトルを算出
+			toDirection = playerObj.transform.position - transform.position;
+			// 対象物へ回転する
+			transform.rotation = Quaternion.FromToRotation(Vector3.up, toDirection);
+		}
 		
 	}
 
 	private void FixedUpdate()
 	{
-		if (enemyHp.GetIsDead() == false) 
+
+		if (enemyHp.GetIsDead() == false)
 		{
-			time += Time.deltaTime;
-
-			if (time >= moveChangeTime)
+			//プレイヤー発見時は追跡する
+			if (isLookPlayer)
 			{
-				//ベクトルを作成
-				randomSpeed.x = Random.Range(-1, 1);
-				randomSpeed.y = Random.Range(-1, 1);
+				transform.position += toDirection.normalized * lookSpeed;
+			}
+			//プレイヤー未発見時はランダムに動かす
+			else
+			{
+				time += Time.deltaTime;
 
-				randomSpeed = randomSpeed.normalized;
+				if (time >= moveChangeTime)
+				{
+					//ベクトルを作成
+					randomSpeed.x = Random.Range(-1, 1);
+					randomSpeed.y = Random.Range(-1, 1);
 
+					randomSpeed = randomSpeed.normalized;
 
-				var v = rb.velocity;
+					transform.rotation = Quaternion.FromToRotation(Vector3.up, randomSpeed);
 
-				//作成したベクトルにスピードをかけて移動させる
-				v = randomSpeed * speed;
+					var v = rb.velocity;
 
-				rb.velocity = v;
-				time = 0;
+					//作成したベクトルにスピードをかけて移動させる
+					v = randomSpeed * normalSpeed;
+
+					rb.velocity = v;
+					time = 0;
+				}
 			}
 		}
 		else
@@ -66,5 +93,10 @@ public class EnemyMove : MonoBehaviour
 			rb.velocity = Vector2.zero;
 		}
 		
+	}
+
+	public void SetIsLook(bool flag)
+	{
+		isLookPlayer = flag;
 	}
 }
